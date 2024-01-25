6b8ca1db4d8ab7404005b5cd340e0ada8af35060
parazinshow <victorfretes32@gmail.com>
2024-01-25 01:04:39 -0700

    ...

---
+++ 30daysOfJavaScript/e2695.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function (nums) {
+  this.arr = nums
+}
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function () {
+  return this.arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  )
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function () {
+  return `[${this.arr.join()}]`
+}
+
+const obj1 = new ArrayWrapper([1, 2])
+const obj2 = new ArrayWrapper([3, 4])
+console.log(obj1 + obj2) // 10
+console.log(String(obj1)) // "[1,2]"
+console.log(String(obj2)) // "[3,4]"
